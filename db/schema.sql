76bd76e1d295786d04e4b3e4d4f407ec6c1b535b
Sashab2400 <sashabarrios2002@gmail.com>
2024-08-18 16:19:09 -0400

    Add functionality to prompt user for employee details

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+\c courses_db;
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE ROLE(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL
+  Salary DECIMAL NOT NULL
+  department_id INTEGER NOT NULL ,
+  CONTRAINT CK_ID0 foreign key (department_id) REFERNECES(  DEPARTMENT_ID)
+
+
+)
+CREATE TABLE employee(
+  id SERIAL PRIMARY KEY
+  first_name VARCHAR(30) NOT NULL
+  last_name VARCHAR(30) NOT NULL
+  role_id INTEGER NOT NULL
+  manager_id INTEGER
+  CONSTRAINT CK_ID0 foreign key (role_id) REFERNECES ROLE(id)
+
+)
+
---
+++ index.js
@@ -0,0 +1,92 @@
+const inquirer = require("inquirer");
+const { Pool } = require('pg');
+// Connect to database
+const pool = new Pool(
+    {
+      // Enter PostgreSQL username
+      user: '',
+      // Enter PostgreSQL password
+      password: '',
+      host: 'localhost',
+      database: 'courses_db'
+  },
+  console.log('Connected to the courses_db database!')
+  )
+inquirer
+  .prompt([
+    {
+      type: "list",
+      choices: [
+        "Add Employee",
+        "View all departments",
+        "View all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+        "Exit",
+      ],
+    },
+  ])
+  .then((response) => {
+    switch (response.options) {
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      case "View all departments":
+       viewAllDepartments();
+        break;
+      case "View all roles":
+        viewallroles();
+        break;
+      case "view all employees":
+        viewallemployees();
+        break;
+      case "add a department":
+        addadepartment();
+        break;
+      case "add a role":
+        addarole();
+        break;
+      case "add an employee":
+        addanemployee();
+        break;
+      case "update an employee role":
+        updateanemployeerole();
+        break;
+     
+      default:
+    }
+  });
+
+  function addanemployee(){
+    console.log("addanemployee")
+    inquirer.prompt([{
+        type:input,
+        message:"Enter employee name",
+        name:"firstname"
+    }],
+    {
+        type:input,
+        message:"Enter employee name",
+        name:"lastname"
+    },
+    {
+        type:input,
+        message:"Enter employee role",
+        name:"role"
+    },
+    {
+        type:input,
+        message:"Enter employee manager",
+        name:"manager"
+    },
+    {
+        type:input,
+        message:"Enter employee manager",
+        name:"manager"
+    },
+)
+  }
